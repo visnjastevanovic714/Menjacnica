4d7ff1205a44fabd8c8583141ecc6e5647d2bb1d
visnjastevanovic714 <visnja.stevanovic5@gmail.com>
2016-03-24 17:45:26 +0100

    Uvedena nova grana specifikacijainterfejsa, implementirane metode klase Menjacnica.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,25 +1,43 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.interfejs.MenjacnicaInterface;
 
 public class Menjacnica implements MenjacnicaInterface {
 
+	LinkedList<Valuta> valute = new LinkedList<Valuta>();
+	
 	@Override
 	public void dodajKurs(String naziv, Kurs kurs) {
-		
+		for(int i=0;i<valute.size();i++){
+			if(valute.get(i).getNaziv().equals(naziv))
+				valute.get(i).getKursevi().add(kurs);
+		}
 	}
 
 	@Override
 	public void obrisiKurs(String naziv, Kurs kurs) {
-
+		for(int i=0;i<valute.size();i++){
+			if(valute.get(i).getNaziv().equals(naziv))
+				valute.get(i).getKursevi().remove(kurs);
+		}
 	}
 
 	@Override
 	public Kurs pronadjiVratiKurs(String naziv, GregorianCalendar dan) {
-		
-		return null;
+		Kurs k = new Kurs();
+		for(int i=0;i<valute.size();i++){
+			if(valute.get(i).getNaziv().equals(naziv)){
+				for(int j=0;j<valute.get(i).getKursevi().size();j++){
+					if(valute.get(i).getKursevi().get(j).getDatum().equals(dan))
+						k = valute.get(i).getKursevi().get(j);
+				}			
+			}
+				
+		}
+		return k;
 	}
 
 }
