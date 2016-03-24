f311b739429eb784adb0801a9c3b6438976615f5
visnjastevanovic714 <visnja.stevanovic5@gmail.com>
2016-03-24 16:43:26 +0100

    Uvedene klase Valuta i Kurs sa odgovarajucim atributima, kao i get i set metode za ove atribute.

---
+++ Menjacnica/src/menjacnica/Kurs.java
@@ -0,0 +1,37 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+public class Kurs {
+	
+	private double prodajni;
+	private double srednji;
+	private double kupovni;
+	private GregorianCalendar datum;
+	
+	public double getProdajni() {
+		return prodajni;
+	}
+	public void setProdajni(double prodajni) {
+		this.prodajni = prodajni;
+	}
+	public double getSrednji() {
+		return srednji;
+	}
+	public void setSrednji(double srednji) {
+		this.srednji = srednji;
+	}
+	public double getKupovni() {
+		return kupovni;
+	}
+	public void setKupovni(double kupovni) {
+		this.kupovni = kupovni;
+	}
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+
+}
---
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -0,0 +1,22 @@
+package menjacnica;
+
+public class Valuta {
+
+	private String naziv;
+	private String skracenica;
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getSkracenica() {
+		return skracenica;
+	}
+	public void setSkracenica(String skracenica) {
+		this.skracenica = skracenica;
+	}
+	
+	
+}
