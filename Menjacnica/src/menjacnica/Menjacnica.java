c8a17ab8f5cd2f05c6e7f81dd8824f2295a758c8
visnjastevanovic714 <visnja.stevanovic5@gmail.com>
2016-03-24 17:33:18 +0100

    Napravljena klasa Menjacnica koja implementira interfejs MenjacnicaInterface.

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,25 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.interfejs.MenjacnicaInterface;
+
+public class Menjacnica implements MenjacnicaInterface {
+
+	@Override
+	public void dodajKurs(String naziv, Kurs kurs) {
+		
+	}
+
+	@Override
+	public void obrisiKurs(String naziv, Kurs kurs) {
+
+	}
+
+	@Override
+	public Kurs pronadjiVratiKurs(String naziv, GregorianCalendar dan) {
+		
+		return null;
+	}
+
+}
